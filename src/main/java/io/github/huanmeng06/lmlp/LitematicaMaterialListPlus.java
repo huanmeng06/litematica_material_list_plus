933bb29f6469809d26a7ccbbd9eb2d17c6bf3da7
Huan_meeng <63433167+huanmeng06@users.noreply.github.com>
2026-07-15 18:56:10 +0800

    Add reproducible builds and build metadata

--- src/main/java/io/github/huanmeng06/lmlp/LitematicaMaterialListPlus.java
+++ src/main/java/io/github/huanmeng06/lmlp/LitematicaMaterialListPlus.java
@@ -2,14 +2,23 @@ package io.github.huanmeng06.lmlp;
 
 import fi.dy.masa.malilib.event.InitializationHandler;
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LitematicaMaterialListPlus implements ClientModInitializer {
     public static final String MOD_ID = "litematica_material_list_plus";
     public static final String MOD_NAME = "Litematica Material List Plus";
-    public static final String MOD_VERSION = "1.8.0+mc1.21.1";
+    public static final String MOD_VERSION = BuildInfo.VERSION;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
+        LOGGER.info("[LMLP build] version={} minecraft={} git={} dirty={} builtAt={}",
+                BuildInfo.VERSION,
+                BuildInfo.MINECRAFT,
+                BuildInfo.GIT_COMMIT,
+                BuildInfo.GIT_DIRTY,
+                BuildInfo.BUILD_TIME);
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
     }
 }
