fd1e3edf1b28a3a0aa86b5e1d951a9c0d865318b
Huan_meeng <63433167+huanmeng06@users.noreply.github.com>
2026-07-19 17:11:12 +0800

    feat: add glazed terracotta preferences

--- src/main/java/io/github/huanmeng06/lmlp/config/TerracottaMaterial.java
+++ src/main/java/io/github/huanmeng06/lmlp/config/TerracottaMaterial.java
@@ -20,7 +20,23 @@ public enum TerracottaMaterial implements IConfigOptionListEntry {
     BROWN("brown", "brown_terracotta"),
     GREEN("green", "green_terracotta"),
     RED("red", "red_terracotta"),
-    BLACK("black", "black_terracotta");
+    BLACK("black", "black_terracotta"),
+    WHITE_GLAZED("white_glazed", "white_glazed_terracotta"),
+    ORANGE_GLAZED("orange_glazed", "orange_glazed_terracotta"),
+    MAGENTA_GLAZED("magenta_glazed", "magenta_glazed_terracotta"),
+    LIGHT_BLUE_GLAZED("light_blue_glazed", "light_blue_glazed_terracotta"),
+    YELLOW_GLAZED("yellow_glazed", "yellow_glazed_terracotta"),
+    LIME_GLAZED("lime_glazed", "lime_glazed_terracotta"),
+    PINK_GLAZED("pink_glazed", "pink_glazed_terracotta"),
+    GRAY_GLAZED("gray_glazed", "gray_glazed_terracotta"),
+    LIGHT_GRAY_GLAZED("light_gray_glazed", "light_gray_glazed_terracotta"),
+    CYAN_GLAZED("cyan_glazed", "cyan_glazed_terracotta"),
+    PURPLE_GLAZED("purple_glazed", "purple_glazed_terracotta"),
+    BLUE_GLAZED("blue_glazed", "blue_glazed_terracotta"),
+    BROWN_GLAZED("brown_glazed", "brown_glazed_terracotta"),
+    GREEN_GLAZED("green_glazed", "green_glazed_terracotta"),
+    RED_GLAZED("red_glazed", "red_glazed_terracotta"),
+    BLACK_GLAZED("black_glazed", "black_glazed_terracotta");
 
     private final String id;
     private final String blockPath;
